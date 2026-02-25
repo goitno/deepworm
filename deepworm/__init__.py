e23cd1a7dfac0681b0c47b0172aed2a87a0ca4f2
Kadir Can Ozden <101993364+bysiber@users.noreply.github.com>
2026-02-25 08:53:54 +0300

    refactor: update __init__.py with all new module exports
    - Added 25 new exports: keywords, footnotes, export hub, summary,
      readability, progress modules
    - Total public API: 65 exports
    - All 656 tests passing

--- deepworm/__init__.py
+++ deepworm/__init__.py
@@ -20,13 +20,19 @@ from .exceptions import (
     SearchError,
     SessionError,
 )
+from .export import ExportFormat, ExportOptions, ExportResult, batch_export, export_report
+from .footnotes import FootnoteResult, add_footnotes, merge_footnotes, renumber_footnotes, strip_footnotes
 from .history import HistoryEntry
+from .keywords import Keyword, KeywordResult, extract_keywords, extract_tags
 from .languages import Language, get_language, list_languages
 from .notion import NotionBlock, NotionPage, export_notion_json, markdown_to_notion
 from .outline import OutlineSection, ReportOutline, generate_outline, outline_from_report
 from .planner import ResearchPlan, generate_plan, estimate_complexity
+from .progress import ProgressSnapshot, ProgressTracker, ResearchStage
+from .readability import ReadabilityResult, analyze_readability
 from .researcher import DeepResearcher
 from .scoring import QualityScore, score_report
+from .summary import Summary, extract_key_findings, extract_topics, summarize
 from .validator import ValidationResult, validate_topic
 
 __all__ = [
@@ -41,36 +47,59 @@ __all__ = [
     "Event",
     "EventEmitter",
     "EventType",
+    "ExportFormat",
+    "ExportOptions",
+    "ExportResult",
+    "FootnoteResult",
     "HistoryEntry",
+    "Keyword",
+    "KeywordResult",
     "Language",
     "ModelNotFoundError",
     "NetworkError",
     "NotionBlock",
     "NotionPage",
     "OutlineSection",
+    "ProgressSnapshot",
+    "ProgressTracker",
     "ProviderError",
     "QualityScore",
     "RateLimitError",
+    "ReadabilityResult",
     "ReportOutline",
     "ResearchPlan",
+    "ResearchStage",
     "SearchError",
     "SessionError",
+    "Summary",
     "ValidationResult",
     "__version__",
+    "add_footnotes",
+    "analyze_readability",
     "async_research",
+    "batch_export",
     "estimate_complexity",
     "export_notion_json",
+    "export_report",
+    "extract_key_findings",
+    "extract_keywords",
+    "extract_tags",
+    "extract_topics",
     "generate_outline",
     "generate_plan",
     "get_language",
     "list_languages",
     "markdown_to_notion",
+    "merge_footnotes",
     "outline_from_report",
+    "renumber_footnotes",
     "research",
     "research_chain",
     "score_report",
     "score_source",
     "score_sources",
+    "strip_footnotes",
+    "summarize",
     "validate_topic",
 ]
 
